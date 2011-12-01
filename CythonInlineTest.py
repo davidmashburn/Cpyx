3bdc176054d2635cc077e38cd30dc447863e5ac0
David Mashburn <david.n.mashburn@gmail.com>
2011-12-01 16:17:46 -0600

    Dec2011

--- CythonInlineTest.py
+++ CythonInlineTest.py
@@ -1,6 +1,26 @@
+import os
+os.environ['MYPYREX']=os.path.expanduser('~/Test') # if you want to compile a file instead of 
+
 import Cpyx
+import numpy as np
 
 # It's magic ;)
 exec(Cpyx.CythonInline('''
-print 'Hello Cython!'
+print 'Hello Cython 2!'
+''',useDistutils=True))
+
+# And it supports numpy ;)
+exec(Cpyx.CythonInline('''
+import numpy as np
+cimport numpy as np
+def f(np.ndarray[np.int_t,ndim=1] a):
+    cdef int i
+    cdef int aLen = a.shape[0]
+    for i in range(aLen):
+        a[i]=a[i]*2
 '''))
+#''',useDistutils=True)) # replace above line with this to use distutils instead
+
+arr = np.array([10,35,50],dtype=np.int)
+f(arr)
+print arr
